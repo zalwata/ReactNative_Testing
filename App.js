234a474e618eef42e04b4321fed9f6a9543c430e
zalwata <zalwata17@gmail.com>
2020-05-08 01:12:26 -0700

    added TodoItem file

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './components/header'
+import TodoItem from './components/todos'
 
 export default function App() {
 
   const [todos, setTodos] = useState([
-    {text: 'buy coffee!!!!!!!', key : '1'},
+    {text: 'buy coffee', key : '1'},
     {text: 'create an app', key : '2'},
     {text: 'play on the switch', key : '3'},
   ]);
+
+  const pressHandler = (key) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.key != key);
+    });
+  };
 
   return (
     <View style={styles.container}>
@@ -21,7 +28,9 @@ export default function App() {
           <FlatList
             data = { todos }
             renderItem = {({ item }) => (
-              <Text>{ item.text }</Text>
+              <TodoItem item = {item} pressHandler ={pressHandler}>
+                { item.text }
+              </TodoItem>
             )}
           />
         </View>
