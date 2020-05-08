c55b8f0bba27a84bd095c2f600b5c2bc2313f522
zalwata <zalwata17@gmail.com>
2020-05-07 23:11:31 -0700

    useState and FlatList

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [todos, setTodos] = useState([
+    {text: 'buy coffee', key : '1'},
+    {text: 'create an app', key : '2'},
+    {text: 'play on the switch', key : '3'},
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!!!!!!</Text>
+
+      {/* {header} */}
+      <View style ={ styles.content} >
+        {/* {to form} */}
+        <View>
+          <FlatList
+            data = {todos}
+            renderItem = {({item}) => (
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,7 +32,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  content: {
+    flex: 1,
+    backgroundColor: '#fff',
   },
 });
